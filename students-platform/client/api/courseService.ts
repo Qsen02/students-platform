ce53c8cdbbab4ea19f928bc778e9e15fd7f08444
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-02-25 22:19:01 +0200

    course service implemented

---
+++ students-platform/client/api/courseService.ts
@@ -0,0 +1,38 @@
+import { Course } from "@/types/course";
+import { del, get, post, put } from "./requester";
+
+const endpoint="courses";
+
+export async function getAllCourses(){
+    const courses = await get(`${endpoint}`);
+    return courses as Course[];
+}
+
+export async function getCourseById(courseId:number){
+    const course = await get(`${endpoint}/${courseId}`);
+    return course as Course;
+}
+
+export async function pagination(page:number){
+    const courses = await get(`${endpoint}/page/${page}`);
+    return courses as Course[];
+}
+
+export async function searchCourses(query:string){
+    const courses = await get(`${endpoint}/search/${query}`);
+    return courses as Course[] | [];
+}
+
+export async function createCourse(data:object){
+    const newCourse = await post(`${endpoint}`,data);
+    return newCourse as Course;
+}
+
+export async function deleteCourse(courseId:number){
+    await del(`${endpoint}/${courseId}`);
+}
+
+export async function editCourse(courseId:number,data:object){
+    const updatedCourse = await put(`${endpoint}/${courseId}`,data);
+    return updatedCourse as Course;
+}
