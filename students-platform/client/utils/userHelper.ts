68ceb736876e4fdff355942a2edcac0dc3710476
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-02-25 21:16:36 +0200

    Implemented user helper functions

---
+++ students-platform/client/utils/userHelper.ts
@@ -0,0 +1,42 @@
+import { UserForAuth } from "@/types/user";
+import EncryptedStorage from "react-native-encrypted-storage";
+
+export async function setUserData(key: string, data: UserForAuth) {
+    try {
+        await EncryptedStorage.setItem(key, JSON.stringify(data));
+    } catch (err) {
+        if (err instanceof Error) {
+            throw new Error(err.message);
+        } else {
+            throw new Error("Unknown error occurd");
+        }
+    }
+}
+
+export async function getUserData(key: string) {
+    try {
+        const data = await EncryptedStorage.getItem(key);
+        if (data) {
+            return JSON.parse(data);
+        }
+        return data;
+    } catch (err) {
+        if (err instanceof Error) {
+            throw new Error(err.message);
+        } else {
+            throw new Error("Unknown error occurd");
+        }
+    }
+}
+
+export async function removeUserData(key:string){
+    try {
+       await EncryptedStorage.removeItem(key);
+    } catch (err) {
+        if (err instanceof Error) {
+            throw new Error(err.message);
+        } else {
+            throw new Error("Unknown error occurd");
+        }
+    }
+}
