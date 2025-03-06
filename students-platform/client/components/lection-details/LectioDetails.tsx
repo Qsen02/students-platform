a03d9ff4e709d5807ecf463612bc396954e8d9ab
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-03-06 10:32:41 +0200

    Update LectioDetails.tsx

--- students-platform/client/components/lection-details/LectioDetails.tsx
+++ students-platform/client/components/lection-details/LectioDetails.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import SelectDropdown from "react-native-select-dropdown";
 import { useUserContext } from "@/context/userContext";
 import { useGetOneLection } from "@/hooks/useLections";
+import Spinner from "react-native-loading-spinner-overlay";
+import { homeStyles } from "../home/HomeStyles";
 
 export default function LectionDetails() {
     const route = useRoute<RouteProp<Routes, "LectionDetails">>();
@@ -13,10 +15,16 @@ export default function LectionDetails() {
     const { user } = useUserContext();
     const [fontSize, setFontSize] = useState("Normal");
     const sizes = ["Small", "Normal", "Big"];
-    const {lection,loading,error}=useGetOneLection(null,lectionId);
+    const { lection, loading, error } = useGetOneLection(null, lectionId);
 
     return (
         <>
+            <Spinner
+                visible={loading}
+                animation="fade"
+                color="rgb(0, 157, 255)"
+                size="large"
+            />
             <View style={LectionDetailsStyles.buttonWrapper}>
                 {lectorId == user?.id ? (
                     <>
@@ -71,57 +79,32 @@ export default function LectionDetails() {
                     }}
                 />
             </View>
-            <ScrollView style={LectionDetailsStyles.lectionWrapper}>
-                <Text style={LectionDetailsStyles.lectionTitle}>
-                    React componentes
-                </Text>
-                <Text
-                    style={
-                        fontSize == "Small"
-                            ? LectionDetailsStyles.smallContent
-                            : fontSize == "Normal"
-                            ? LectionDetailsStyles.normalContent
-                            : fontSize == "Big"
-                            ? LectionDetailsStyles.bigContent
-                            : ""
-                    }
-                >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quod ad mollitia iste laboriosam quibusdam, delectus veniam
-                    ut nihil itaque doloribus consequuntur corporis, nisi
-                    maiores saepe aperiam temporibus sit nesciunt incidunt
-                    reiciendis cum sequi? Earum vel dolores sapiente, veniam
-                    perferendis, dolorum dolor cum, molestiae voluptatem ipsam
-                    error sed ipsa fugit nisi beatae reiciendis! Quidem, sit
-                    laudantium aperiam delectus, dignissimos molestias quo nulla
-                    tempore consequuntur a aliquam illum possimus laborum ex est
-                    minima commodi expedita adipisci perspiciatis explicabo
-                    aliquid aspernatur? Architecto quae similique labore
-                    reprehenderit inventore doloremque magnam alias nihil iste
-                    aliquid. Sapiente est illo eligendi nisi necessitatibus
-                    doloribus error eaque at reprehenderit deserunt tenetur
-                    vitae doloremque, rem commodi id dolorum rerum magni quaerat
-                    voluptatem aspernatur impedit neque architecto assumenda
-                    nesciunt? Repudiandae possimus beatae, iure dignissimos
-                    inventore fuga velit adipisci, voluptatum neque quo itaque
-                    ipsam officia quas asperiores tenetur saepe! Et harum quia
-                    dignissimos? Est qui, eius doloribus sint, quo neque odit,
-                    excepturi accusantium nam dolor corrupti! Odit ad, veniam
-                    rem voluptatem expedita iusto laboriosam aut ab repellendus
-                    quos quia eveniet repellat obcaecati, voluptatum quam?
-                    Pariatur odio, iusto commodi, rerum, at veritatis excepturi
-                    earum esse et molestias delectus distinctio recusandae.
-                    Doloribus quis eos doloremque ducimus. Magni veritatis,
-                    debitis sequi magnam odio omnis est voluptate pariatur quo?
-                    Pariatur tempore in ea numquam a? Tenetur, quos sit!
-                    Perspiciatis soluta, voluptate sit eligendi facilis at sint
-                    officia incidunt blanditiis unde corrupti cum ratione totam
-                    dolorem illum, nobis itaque odio praesentium quas ullam
-                    magnam nesciunt possimus. Similique, doloremque minima cum
-                    officiis soluta iusto adipisci eveniet! Ad aspernatur
-                    blanditiis modi ratione illum.f
-                </Text>
-            </ScrollView>
+            {error ? (
+                <View style={homeStyles.error}>
+                    <Text style={homeStyles.errorText}>
+                        Server is not responding, please try again later.
+                    </Text>
+                </View>
+            ) : (
+                <ScrollView style={LectionDetailsStyles.lectionWrapper}>
+                    <Text style={LectionDetailsStyles.lectionTitle}>
+                        {lection?.lectionName}
+                    </Text>
+                    <Text
+                        style={
+                            fontSize == "Small"
+                                ? LectionDetailsStyles.smallContent
+                                : fontSize == "Normal"
+                                ? LectionDetailsStyles.normalContent
+                                : fontSize == "Big"
+                                ? LectionDetailsStyles.bigContent
+                                : ""
+                        }
+                    >
+                        {lection?.content}
+                    </Text>
+                </ScrollView>
+            )}
         </>
     );
 }
