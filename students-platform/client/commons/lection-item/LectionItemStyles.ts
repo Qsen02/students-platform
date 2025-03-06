8ed469254bf5972963cdc50cce420a67d6b28a9f
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-03-06 11:42:21 +0200

    Update LectionItemStyles.ts

--- students-platform/client/commons/lection-item/LectionItemStyles.ts
+++ students-platform/client/commons/lection-item/LectionItemStyles.ts
@@ -14,6 +14,7 @@ export const lectionItemStyles=StyleSheet.create({
         alignItems:"center"
     },
     lectionText:{
+        flexBasis:"40%",
         fontSize:16,
         color:"white",
         textAlign:"center"
